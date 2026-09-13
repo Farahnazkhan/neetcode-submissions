7540c50454741005fabd306f26cd8869b4f91261
Farahnazkhan <93282260+Farahnazkhan@users.noreply.github.com>
2026-09-14 00:44:59 +0500

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isDuplicate(self, array: List) -> bool:
+        compare = set()
+        for item in array:
+            if item != '.' and item in compare:
+                return True
+            compare.add(item)
+        return False
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        # check for rows
+        for row in board:
+            if self.isDuplicate(row):
+                return False
+        
+        #check for columns
+        for i in range(len(board[0])):
+            col = [row[i] for row in board]
+            if self.isDuplicate(col):
+                return False
+        
+        #check for squares
+        squares = [
+            [board[r][c] for r in range(i, i + 3) for c in range(j, j + 3)]
+            for i in range(0, 9, 3)
+            for j in range(0, 9, 3)
+        ]
+        for square in squares:
+            if self.isDuplicate(square):
+                return False
+
+        return True
+        
